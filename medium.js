943eb685ad2e41b338036dc2c09adb4db0612233
Balexm95 <bria.mccoy95@gmail.com>
2022-01-24 21:44:28 -0500

    Final Commit

--- medium.js
+++ medium.js
@@ -8,13 +8,13 @@
 
 //console.log(sharePizza(3)); 
 
-  // prints "Each person gets 2.67 slices of pizza"
-  var sharePizza = cutPizzaSlices(8);
+  // prints "Each person gets 2.67
 
-  function cutPizzaSlices(x){
-      function sharePizza(y){
-        console.log("Each person gets " + x/y + " slices of pizza.");
-      }
-      console.log(sharePizza(2));
-      console.log(sharePizza(3));
-  }
+var sharePizza = function pcutPizzaSlices(x,y=8){ 
+  // here one argument pass from caller and one is by default (y=8)
+  
+ return 'Each person gets '+(y/x).toFixed(2)+' slices of pizza';
+// this expression calculation for desired result, toFixed(2) method is used for get the valu till two decimal point
+}
+console.log(sharePizza(2)); // printing result returned by function expression
+console.log(sharePizza(3));
